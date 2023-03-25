ab08347bb72a8bd6195a092298cd723221edae24
Carla Ruiz <carla@dhcp-10-30-78-164.utsarr.net>
2023-03-25 14:41:16 -0500

    Initial commit

---
+++ app/src/main/java/edu/utsa/andriod/studybuddy/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.utsa.andriod.studybuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
